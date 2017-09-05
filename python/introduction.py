b7d0630172c5860d75a43f6a5952337ac3bf9d98
Steven Chavez <schavez256@yahoo.com>
2017-09-04 18:00:10 -0600

    Added introduction with notes

---
+++ python/introduction.py
@@ -0,0 +1,20 @@
+# Author: Steven Chavez
+# Email: steven@stevenscode.com
+# Date: 9/3/2017
+# File: introduction.py
+# Resource: Python Introduction. (2016).  Retrieved from https://developers.google.com/edu/python/introduction
+
+
+'''
+NOTES
+
+Python throws an error if the code tries to read an unassigned 
+variable. 
+
+Python variables are case sensitive.
+
+Unlike other languages. Python does not use semicolons to end
+a statement. Python simply uses the end of the line.
+
+Python files end in .py and are called modules.
+'''
