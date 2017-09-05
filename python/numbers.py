e3653c8219d14b02bbdc30a80811a73c09f6cb22
Steven Chavez <schavez256@yahoo.com>
2017-09-04 18:28:50 -0600

    simple math equation

---
+++ python/numbers.py
@@ -0,0 +1,16 @@
+# Author: Steven Chavez
+# Email: steven@stevenscode.com
+# Date: 9/4/2017
+# File: numbers.py
+
+
+''' Expression is used like most other languages.
+you are able to use the operators +, -, *, and /
+like you would normaly use them. You can also group 
+the expressions with parantheses ()'''
+
+# simple addition, subtraction, multiplication, and division
+print 1 + 1
+print 4 - 2
+print 1 * 2
+print 4 / 2
