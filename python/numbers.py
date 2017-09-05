ef75bbb513390006cf7b90e3deb5ad51e5a4f673
Steven Chavez <schavez256@yahoo.com>
2017-09-04 19:04:48 -0600

    Added floor division examples

--- python/numbers.py
+++ python/numbers.py
@@ -6,11 +6,21 @@
 
 ''' Expression is used like most other languages.
 you are able to use the operators +, -, *, and /
-like you would normaly use them. You can also group 
-the expressions with parantheses ()'''
+like you would normally use them. You can also group 
+the expressions with parentheses ()'''
 
-# simple addition, subtraction, multiplication, and division
-print 1 + 1
-print 4 - 2
-print 1 * 2
-print 4 / 2
+# addition, subtraction, multiplication
+1 + 1 # = 2
+4 - 2 # = 2
+1 * 2 # = 2
+
+# division always returns a floating point number
+# whole numbers are type int decimals (5.3, 3.0) are floats
+6 / 5 # = 1.2
+
+# equations using parentheses grouping
+(2 + 5) * (40-35) # = 35
+
+# floor division (//)
+11 / 2 # = 5.5 normal division 
+11 // 2 # = 5 floor division gets rid of the fraction
