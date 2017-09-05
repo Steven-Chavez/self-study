22e7f0dac988761e0d5548fd9dc6f0377ed14ca8
Steven Chavez <schavez256@yahoo.com>
2017-09-05 12:08:39 -0600

    Added basic list format example

---
+++ python/introduction/lists.py
@@ -0,0 +1,12 @@
+# Author: Steven Chavez
+# Email: steven@stevenscode.com
+# Date: 9/4/2017
+# File: strings.py
+# Resource: https://docs.python.org/3/tutorial/introduction.html
+
+# Lists are used to group togther values.
+
+# Basic list format
+numbers = [2, 5, 10, 33]
+names = ['Tesla', 'Edison', 'Nikola']
+
