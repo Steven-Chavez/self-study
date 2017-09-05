1d950b06a500822bb9d8782465e239b1820cc917
Steven Chavez <schavez256@yahoo.com>
2017-09-04 21:21:47 -0600

    escaping quotes

--- python/strings.py
+++ python/strings.py
@@ -2,3 +2,11 @@
 # Email: steven@stevenscode.com
 # Date: 9/4/2017
 # File: strings.py
+
+''' Strings can be in closed with single quotes '..'
+or with double quotes "..". What you decide to use is
+up to you there is no difference. Use \ to escape. '''
+
+print('doesn\'t') # escape single quote with \'
+print("doesn't") # if you use double quotes escaping is unessary
+
