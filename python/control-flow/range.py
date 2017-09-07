863cb1224d40eb1fc814e29d9069ececb4a64314
Steven Chavez <schavez256@yahoo.com>
2017-09-06 18:08:36 -0600

    range() starting number example

--- python/control-flow/range.py
+++ python/control-flow/range.py
@@ -8,4 +8,10 @@
 
 # Combining a for statement and the range() function acts
 # more like a traditional for loop
+for i in range(10):
+	print(i)
+	
+print()
+# You are able to set starting number
+for i in range(5, 11):
 	print(i)
