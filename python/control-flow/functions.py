33ea93000cfd78bbe6adfa520aae68c2edc81965
Steven Chavez <schavez256@yahoo.com>
2017-09-10 18:21:13 -0600

    Added default argument examples

--- python/control-flow/functions.py
+++ python/control-flow/functions.py
@@ -13,3 +13,12 @@ def addition(n, m):
 total = addition(20, 80) # Call function
 print(total)
 print(addition.__doc__) # Displays functions docstring
+
+# Defualt argument values allow you to set default values
+def subtraction(n = 9, m = 6):
+    '''Subtract both arguments'''
+    sum = n - m
+    return sum
+
+total = subtraction()
+print(total)
