5778de9cde135d872908060e78dbd991d4797a6a
Steven Chavez <schavez256@yahoo.com>
2017-09-11 22:29:31 -0600

    Added extend() example

--- python/data-structures/more-on-lists.py
+++ python/data-structures/more-on-lists.py
@@ -2,4 +2,18 @@
 # Email: steven@stevenscode.com
 # Date: 9/11/2017
 # File: more-on-lists.py
-# Resource:https://docs.python.org/3/tutorial/datastructures.html
+# Resource:https://docs.python.org/3/tutorial/datastructures.htm
+
+# This document will cover all of the methods for lists
+name = ['Tesla', 'Steven', 'Jaky', 'Breanna', 'Alicia']
+
+# Add an item to the end of list
+name.append('Pedro')
+print(name)
+
+# Use extend() method
+name[len(name):] = name
+print(name)
+
+name[len(name):] = ['Georgia', 'Helen']
+print(name)
