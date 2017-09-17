2c9c2b9d4988fd1ac5780b949dec59a8a1f5b197
Steven Chavez <schavez256@yahoo.com>
2017-09-16 22:16:23 -0600

    Added list for testing

--- python/data-structures/del.py
+++ python/data-structures/del.py
@@ -7,3 +7,8 @@
 '''Unlike the pop() method the del statement does not return
 a value. It simply deletes the item at the given index or
 slice expression'''
+
+# initialize list for testing using list comprehension
+test = [x**2 for x in range(1,11)]
+print(test)
+
