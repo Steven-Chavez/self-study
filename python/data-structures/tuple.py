548c41c8a4c5c6f743f1b1aa6c3309dce5cdedd8
Steven Chavez <schavez256@yahoo.com>
2017-09-17 09:47:13 -0600

    Added comment header to file

---
+++ python/data-structures/tuple.py
@@ -0,0 +1,9 @@
+# Author: Steven Chavez
+# Email: steven@stevenscode.com
+# Date: 9/17/17
+# File: tuple.py
+# Resource: https://docs.python.org/3/tutorial/datastructures.html#tuples-and-sequences
+
+# Tuples and Sequences
+'''A tuple is an immutable container that is made up of a number
+of values separated by commas'''
