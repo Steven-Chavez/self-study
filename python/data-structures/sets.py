942af164aecc846a0dded124b4ae62f36914724e
Steven Chavez <schavez256@yahoo.com>
2017-09-19 22:20:35 -0600

    Added file to show examples of sets

---
+++ python/data-structures/sets.py
@@ -0,0 +1,9 @@
+# Author: Steven Chavez
+# Email: steven@stevenscode.com
+# Date: 9/19/2017
+# File: sets.py
+# Resource: https://docs.python.org/3/tutorial/datastructures.html#tuples-and-sequences
+
+# Sets
+'''A set is an unordered collection with no duplicate item'''
+
