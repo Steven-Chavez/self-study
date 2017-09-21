5c8ab6feed16946f3d85090749459235a6b3f503
Steven Chavez <schavez256@yahoo.com>
2017-09-20 20:53:20 -0600

    Added comment header for dictionary examples

---
+++ python/data-structures/dic.py
@@ -0,0 +1,14 @@
+# Author: Steven Chavez
+# Email: steven@stevenscode.com
+# Date: 9/20/2017
+# File: dic.py
+# Resource: https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+
+# Dictionaries
+''' Dictionaries are usually seen in other languages as
+as associative arrays. Dictionaries are made up of keys 
+and values. Instead of indexes and values. Keys have to 
+remain immutable. Keys have to remain unique per 
+dictionary.'''
+
+
