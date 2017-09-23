30192c4a834cb258555fbf1c7e1492cf7ca25bdc
Steven Chavez <schavez256@yahoo.com>
2017-09-22 20:38:58 -0600

    Added comment header to modules.py

---
+++ python/modules/modules.py
@@ -0,0 +1,7 @@
+# Author: Steven Chavez
+# Email: steven@stevenscode.com
+# Date: 9/22/2017
+# File: modules.py
+# Resource:https://docs.python.org/3/tutorial/modules.htm://docs.python.org/3/tutorial/modules.html
+
+# Modules
