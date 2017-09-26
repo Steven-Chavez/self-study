685918e339e0e7f47a2d98eacf3c2727b79c0fe5
Steven Chavez <schavez256@yahoo.com>
2017-09-26 15:19:05 -0600

    Added file to show module examples

--- python/modules/modules.py
+++ python/modules/modules.py
@@ -5,3 +5,12 @@
 # Resource:https://docs.python.org/3/tutorial/modules.htm://docs.python.org/3/tutorial/modules.html
 
 # Modules
+''' A module is a file. This file contains Python statements
+and definitions.'''
+
+# In order to use other module functions you import the modue
+import header
+
+# By using the module name you can access the functions.
+header = header.header('Steven', 'S@yahoo.com', 'Sept 22, 2017')
+print(header)
