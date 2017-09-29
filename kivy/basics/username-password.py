7df50b55db00d864d5e555f7e5f84df392188ce9
Steven Chavez <schavez256@yahoo.com>
2017-09-28 19:32:39 -0600

    Extend application

--- kivy/basics/username-password.py
+++ kivy/basics/username-password.py
@@ -4,3 +4,8 @@
 # File: username-password.py
 # Resource: https://kivy.org/docs/guide/basic.html
 
+# Extend application
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
