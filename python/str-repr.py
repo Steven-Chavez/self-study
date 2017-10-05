5019d66ef6ecb419fcb1510c71caa6758ee1c8f4
Steven Chavez <schavez256@yahoo.com>
2017-10-04 21:09:44 -0600

    Added example definition header

--- python/str-repr.py
+++ python/str-repr.py
@@ -4,3 +4,7 @@
 # File: str-repr.py
 # Resource: https://docs.python.org/3/tutorial/inputoutput.html
 
+# str() and repr()
+'''str() returns a human-readable representation of the 
+values. repr() returns an interpreter-readable representation
+of the values.'''
